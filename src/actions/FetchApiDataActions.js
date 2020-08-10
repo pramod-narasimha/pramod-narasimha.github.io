d48fb89ea4ccd9bd1c5c15efe0e21666052cf389
Pramod N <npramod95@gmail.com>
2020-08-10 14:25:33 +0530

    url changes

--- src/actions/FetchApiDataActions.js
+++ src/actions/FetchApiDataActions.js
@@ -1,42 +1,41 @@
 import axios from "axios";
 const api = {
-  devApi : "http://localhost:3000/apiResponse.json",
-  prodApi : "https://pramod-narasimha.github.io/apiResponse.json"
+  devApi: "http://localhost:3000/apiResponse.json",
+  prodApi: "https://pramod-narasimha.github.io/fetch-bank-list/userData.json",
 };
 
 export function setApiData(data) {
   return {
     type: "SET_API_DATA",
-    payload: data
+    payload: data,
   };
 }
 
 export function setLoading(data) {
   return {
     type: "IS_LOADING",
-    payload: data
+    payload: data,
   };
 }
 
-export function setSideDrawer(data){
+export function setSideDrawer(data) {
   return {
     type: "SET_DRAWER",
-    payload: data
-  }
-
+    payload: data,
+  };
 }
-export const fetchApiData = () => dispatch => {
+export const fetchApiData = () => (dispatch) => {
   dispatch(setLoading(true));
-  axios.get(api.devApi).then(response => {
+  axios.get(api.prodApi).then((response) => {
     dispatch(setApiData(response.data.aData));
     dispatch(setLoading(false));
   });
 };
 
-export const openSideDrawer = () => dispatch => {
+export const openSideDrawer = () => (dispatch) => {
   dispatch(setSideDrawer("open"));
-}
+};
 
-export const closeSideDrawer = () => dispatch => {
+export const closeSideDrawer = () => (dispatch) => {
   dispatch(setSideDrawer("close"));
-}
+};
